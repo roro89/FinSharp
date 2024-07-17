8e657b43f65d13d50e3474c79b6a971e9ccab17a
Romanas <romanasdev@gmail.com>
2024-07-17 23:14:45 +0300

    Debugging...

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './Routes/Routes';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
